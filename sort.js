ce8ca453159a92f225338a23320cd803c558c948
lutfiqaraman <lutfiqaraman@gmail.com>
2022-01-18 20:36:25 +0200

    sort elements

---
+++ sort.js
@@ -0,0 +1,10 @@
+/*
+        That will do sorting in a new array, while keep the original array as it is
+        without change it
+ */
+
+const items = ["car", "wheels", "motor", "bike"];
+const sortedItems =  [...items].sort();
+
+console.log(items);
+console.log(sortedItems);
