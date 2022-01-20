95e2329297aacfb3d655015b0e2a7edd061c7b35
lutfiqaraman <lutfiqaraman@gmail.com>
2022-01-20 22:30:30 +0200

    Using guard clauses in JS

---
+++ validatecandidate.js
@@ -0,0 +1,22 @@
+/*
+        Using guard clauses in JS
+ */
+
+function getValidatecandidate(candidate, members) {
+    if (!candidate)
+        return;
+
+    const member = members.find(
+        (member) => member.name === candidate
+    );
+
+    if (!member)
+        return;
+
+    const hasLegalAge  = member.age > 18;
+
+    if (!hasLegalAge)
+        return;
+
+    return member;
+}
